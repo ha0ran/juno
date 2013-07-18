9625377fc60063ebe0e425b899bf86a8f960d7dc
Haoran Deng <denghr85@gmail.com>
2013-07-18 23:48:38 +0800

    Add Post View

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post("/upload", function (request, response) {
     }));
     response.end();
 });
-   
+
 app.get( '/api/posts', function( request, response ) { 
     var page = request.query.page;
     var limit = request.query.limit;
@@ -77,4 +77,53 @@ app.post( '/api/posts', function( request, response ) {
     });
     return response.send( post ); 
 });
+
+// Get a new post by ID
+app.get( '/api/posts/:id', function( request, response ) {
+    console.log("Get Post by id:" + request.params.id);
+    return Post.findById( request.params.id, function( err, post ) {
+        if( !err ) {
+            console.log(post.id);
+            console.log(post.title);
+            console.log(post.content);
+            return response.send( post );
+        } else {
+            return console.log( err );
+        } 
+    });
+});
+
+//Update a post
+app.put( '/api/posts/:id', function( request, response ) {
+    console.log( 'Updating book ' + request.body.title );
+    return Post.findById( request.params.id, function( err, post ) {
+        post.title = request.body.title;
+        post.content = request.body.content;
+        post.lastModifiedAt = new Date().getTime();
+        return post.save( function( err ) { 
+            if( !err ) {
+                console.log( 'post updated' ); 
+            } else {
+                console.log( err );
+            }
+            return response.send( post ); 
+        });
+    }); 
+});
+
+ //Delete a post
+app.delete( '/api/posts/:id', function( request, response ) { 
+    console.log( 'Deleting book with id: ' + request.params.id );
+    return Post.findById( request.params.id, function( err, post ) {
+        return post.remove( function( err ) {
+            if( !err ) {
+                console.log( 'Book removed' ); 
+                return response.send( '' );
+            } else {
+                console.log( err );
+            }
+        }); 
+    });
+});
+
 app.listen(3000);
